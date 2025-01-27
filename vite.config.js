076e5805cfd7a06a55f60a067bbe4797d3d30af8
inherbver <inherbver@gmail.com>
2025-01-27 18:02:57 +0100

    Ajout des nouvelles modifications et réorganisation des fichiers

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend', // Racine du projet
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend/src'), // Alias pour simplifier les imports
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss, // Importé via ES Modules
+        autoprefixer, // Importé via ES Modules
+      ],
+    },
+  },
+  server: {
+    open: true, // Ouvre automatiquement le navigateur
+    port: 3000, // Définit le port du serveur
+  },
+  build: {
+    outDir: '../dist', // Répertoire de sortie pour le build
+    sourcemap: true, // Active les sourcemaps pour le débogage
+  },
+});
